db3c5aa47d96a2f5f3990cc2544714f1d4af9b5d
leticia794 <129951386+leticia794@users.noreply.github.com>
2024-06-12 08:52:51 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+const lista=["Monica","Arthur","Paulo"]
+for(i=0; i<= lista.lengeth ; i++){
+    
+    if(lista[i]=="arthur"){
+        console.log("existe")
+        console.log("indice:"+i)
+    }
+}
+    if(lista.include("Arthur")){
+        console.log("existe")
+        i=lista.indexOf("arthur")
+        console.log("indice: "+i)
+    }
